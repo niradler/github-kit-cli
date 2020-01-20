e048794d712d28e9834a6200d9870854c1622926
Nir Adler <github@niradler.com>
2020-01-20 20:30:28 +0200

    filter and map

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 const Yargs = require("yargs"); // eslint-disable-line
 const GithubApi = require("./GithubApi");
 const querystring = require("querystring");
+const Configstore = require("configstore");
+const packageJson = require("./package.json");
+
+const config = new Configstore(packageJson.name, {});
 
 const initGithub = argv => {
   const opt = {};
   if (argv.auth) opt.auth = argv.auth;
+  if (config.get("auth")) opt.auth = config.get("auth");
   const githubApi = new GithubApi(opt);
 
   return githubApi;
@@ -22,11 +27,11 @@ const filter = (argv, data) => {
 };
 
 const map = (argv, data) => {
-  const fields = argv.map.split(",");
+  const fields = argv.map.includes(",") ? argv.map.split(",") : [argv.map];
 
   return data.map(d => {
     const obj = {};
-    fields.foreach(f => (obj[f] = d[f]));
+    fields.forEach(f => (obj[f] = d[f]));
     return obj;
   });
 };
@@ -56,7 +61,46 @@ Yargs.command(
       const githubApi = initGithub(argv);
       const res = await githubApi.search(argv.action, { q: argv.q });
       r = res.data;
+      if (argv.map || argv.filter) {
+        r = r.items;
+        if (argv.map) r = map(argv, r);
+        if (argv.filter) r = filter(argv, r);
+      }
+      console.log(r);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+);
 
+Yargs.command(
+  "any [domain] [action]",
+  "query any domain on github sdk",
+  yargs => {
+    yargs.positional("action", {
+      describe: "action",
+      require: true
+    });
+
+    yargs.positional("domain", {
+      describe: "domain",
+      require: true
+    });
+
+    yargs.option("params", {
+      describe: "params",
+      alias: "p",
+      require: true
+    });
+  },
+  async argv => {
+    try {
+      let r = [];
+      if (argv.verbose) console.info("verbose is on.", argv);
+      const githubApi = initGithub(argv);
+      const params = transformParams(argv.params);
+      const res = await githubApi.any(argv.domain, argv.action, params);
+      r = res.data;
       if (argv.map) r = map(argv, r.items);
       if (argv.filter) r = filter(argv, r.items);
       console.log(r);
@@ -64,48 +108,35 @@ Yargs.command(
       console.error(error.message);
     }
   }
-)
-  .command(
-    "any [domain] [action]",
-    "query any domain on github sdk",
-    yargs => {
-      yargs.positional("action", {
-        describe: "action",
-        require: true
-      });
+);
 
-      yargs.positional("domain", {
-        describe: "domain",
-        require: true
-      });
-
-      yargs.option("params", {
-        describe: "params",
-        alias: "p",
-        require: true
-      });
-    },
-    async argv => {
-      try {
-        let r = [];
-        if (argv.verbose) console.info("verbose is on.", argv);
-        const githubApi = initGithub(argv);
-        const params = transformParams(argv.params);
-        const res = await githubApi.any(argv.domain, argv.action, params);
-        r = res.data;
-        if (argv.map) r = map(argv, r.items);
-        if (argv.filter) r = filter(argv, r.items);
-        console.log(r);
-      } catch (error) {
-        console.error(error.message);
-      }
+Yargs.command(
+  "store",
+  "store keys and secret",
+  yargs => {
+    yargs.option("auth", {
+      alias: "a",
+      type: "string",
+      description: "github api key",
+      require: true
+    });
+  },
+  async argv => {
+    try {
+      config.set("auth", argv.auth);
+      console.log(config.all);
+    } catch (error) {
+      if (argv.verbose) console.log(error);
+      else console.error(error.message);
     }
-  )
-  .option("auth", {
-    alias: "a",
-    type: "string",
-    description: "github api key"
-  })
+  }
+);
+
+Yargs.option("auth", {
+  alias: "a",
+  type: "string",
+  description: "github api key"
+})
   .option("filter", {
     alias: "f",
     type: "string",
